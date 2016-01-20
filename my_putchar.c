252390830fd4807132cbafe4fdcd44497c08ff56
rahmeh_d <daniel.rahmeh@epitech.eu>
2016-01-20 17:58:11 +0100

    my_putchar

---
+++ my_putchar.c
@@ -0,0 +1,11 @@
+/*
+** my_putchar.c for  in /home/rahmeh_d/github/lib-c
+** 
+** Started on  Wed Jan 20 17:56:35 2016 Rahmeh Daniel
+** Last update Wed Jan 20 17:57:50 2016 Rahmeh Daniel
+*/
+
+void	my_putchar(char c);
+{
+  write(1, &c, 1);
+}

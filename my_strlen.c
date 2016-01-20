d962206288ad19ca39ce8babe0394ceaaedf0bc2
rahmeh_d <daniel.rahmeh@epitech.eu>
2016-01-20 18:33:13 +0100

    my_strlen

---
+++ my_strlen.c
@@ -0,0 +1,15 @@
+/*
+** my_strlen.c for my_strlen in /home/rahmeh_d/github/lib-c
+** 
+** Started on  Wed Jan 20 18:07:08 2016 Rahmeh Daniel
+** Last update Wed Jan 20 18:32:51 2016 Rahmeh Daniel
+*/
+
+int	my_strlen(char *str)
+{
+  int	i;
+  i = 0;
+  while (str[i])
+    i++;
+  return (0);
+}
